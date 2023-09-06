b8bcacca138f58dcac7e758eb22c48d1a27784f4
George Rotteny <george.lopes@safetydocs.com.br>
2023-09-06 17:16:26 -0300

    Inclusão de integração com node por meio de chamada HTTP(s)

--- src/ChartJs.php
+++ src/ChartJs.php
@@ -9,6 +9,13 @@ class ChartJs {
     public $payload             = []; // required
 
     /**
+     * Requisição API
+     */
+    public $api_url             = 'https://sistema.safetydocs.com.br/nodechartjs/';
+    public $api_user            = 'safetydocs';
+    public $api_pass            = '33e3PY26zfXuKB4efQHGtW#V3BI8p1NMT0w1O3Ql1P9Oec@kB7Pg2MZGBtqFQjiVCQ';
+
+    /**
      * Localização da aplicação node que irá executar a geração do grafico
      */
     public $node_path           = "index.js";
@@ -29,6 +36,15 @@ class ChartJs {
         if(isset($params['node_path'])) {
             $this->node_path = $params['node_path'];
         }
+        if(isset($params['api_url'])) {
+            $this->api_url = $params['api_url'];
+        }
+        if(isset($params['api_user'])) {
+            $this->api_user = $params['api_user'];
+        }
+        if(isset($params['api_pass'])) {
+            $this->api_pass = $params['api_pass'];
+        }
     }
     
     /**
@@ -39,14 +55,13 @@ class ChartJs {
            // Testa se o index responde a chamada de teste corretamente
            $str_exec   = "node {$this->node_path} teste";
            
-           $return     = exec($str_exec);
+           $return     = shell_exec($str_exec);
            $returnObj  = json_decode($return);
    
            if(isset($returnObj->status) && $returnObj->status) {
                return true;
            }
         }
-
 
         throw new \Exception("O projeto node não foi encontrado no caminho informado.");
     }
@@ -70,21 +85,63 @@ class ChartJs {
         ]);
         $str_exec   = "node {$this->node_path} '{$chart}'";
         
-        $return     = exec($str_exec);
-        $returnObj  = json_decode($return);
+        $return     = shell_exec($str_exec);
+        $returnObj  = $this->validateObject($return);
 
+        return $returnObj->image;
+    }
+
+    public function getHttpChart() {
+        $chart = urlencode(json_encode([
+            'width'             => $this->width,
+            'height'            => $this->height,
+            'backgroundColour'  => $this->backgroundColour,
+            'payload'           => (object)$this->payload
+        ]));
+        $query  = "chart={$chart}";
+        $url    = "{$this->api_url}?{$query}";
+
+        if(!function_exists("curl_init")) {
+            throw new \Exception("Biblioteca CURL não está habilitada");
+        }
+
+        $ch     = curl_init( $url );
+        curl_setopt( $ch
+                    , CURLOPT_HTTPHEADER
+                    , [
+                        'Authorization: Basic ' . base64_encode( $this->api_user . ':' . $this->api_pass ),
+                        'Content-Type: application/json',
+                        'Content-Length: ' . strlen($query)
+                    ]);
+        $return     = curl_exec($ch);
+        $httpCode   = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close( $ch );
+        if($httpCode != "200") {
+            throw new \Exception("Error ({$httpCode})");
+        }
+
+        $returnObj  = $this->validateObject($return);
+        return $returnObj->image;
+    }
+
+    public function validateObject($return) {
+        $returnObj  = json_decode($return);
         if(!isset($returnObj->status) || !$returnObj->status) {
             if(isset($returnObj->message)) {
                 throw new \Exception($returnObj->message);
             }
             throw new \Exception($return);
         }
-
-        return $returnObj->image;
+        return $returnObj;
     }
 
     public static function renderChart($payload, $width = null, $height = null, $backgroundColour = null, $node_path = null) {
         $ChartJs = new ChartJs(compact('payload', 'width', 'height', 'backgroundColour', 'node_path'));
         return $ChartJs->getChart();
+    }
+
+    public static function renderHttpChart($payload, $width = null, $height = null, $backgroundColour = null, $api_url = null, $api_user = null, $api_pass = null) {
+        $ChartJs = new ChartJs(compact('payload', 'width', 'height', 'backgroundColour', 'api_url', 'api_user', 'api_pass'));
+        return $ChartJs->getHttpChart();
     }
 }
